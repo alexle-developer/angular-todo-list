1dac64ea04a486575232f2ab5fd2ed50571b3aed
Alex Le <ale@americanfirstfinance.com>
2024-03-15 08:56:44 -0500

    add todo interface & mock data

---
+++ apps/ngrx-signal-store/src/app/model/todo-mock-data.ts
@@ -0,0 +1,8 @@
+import { Todo } from './todo.model';
+
+export const todoMockData: Todo[] = [
+  { id: 1, title: 'Buy groceries', completed: false },
+  { id: 2, title: 'Clean the house', completed: true },
+  { id: 3, title: 'Go for a run', completed: false },
+  { id: 4, title: 'Learn PHP', completed: false },
+];
