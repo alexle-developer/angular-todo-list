ef74a26458c0db3c2727b13fd3cbdb7db6d54150
Alex Le <ale@americanfirstfinance.com>
2024-03-21 10:10:36 -0500

    add Todo model and mock data

---
+++ apps/todo/todo-ngrx/src/app/model/todo-mock-data.ts
@@ -0,0 +1,9 @@
+import { Todo } from './todo.model';
+
+export const ToDosData: Todo[] = [
+  { id: 1, title: 'Buy groceries', completed: false },
+  { id: 2, title: 'Walk the dog', completed: false },
+  { id: 3, title: 'Finish homework', completed: true },
+  { id: 4, title: 'Learn PHP', completed: false },
+  // Add more todo items here
+];
