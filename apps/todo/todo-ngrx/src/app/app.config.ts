8222acd5a27f5df3f96b703fd9b2fa26f795c699
alexle-developer <alex.le.developer@gmail.com>
2024-03-24 12:37:58 -0500

    add @ngrx/store-devtools - browser devtools is working

--- apps/todo/todo-ngrx/src/app/app.config.ts
+++ apps/todo/todo-ngrx/src/app/app.config.ts
@@ -4,11 +4,14 @@ import { appRoutes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { appEffects, appStore } from './+state/todo.state';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment'; // Import the 'environment' object
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideRouter(appRoutes),
     provideEffects(appEffects), // register the effects with the NgRx store
     provideStore(appStore), // register the reducers with the NgRx store
-    provideRouter(appRoutes),
+    provideStoreDevtools({ maxAge: 5, logOnly: environment.production }), // enable the NgRx store devtools
   ],
 };
