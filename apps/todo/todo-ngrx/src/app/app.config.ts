5f9d7503a6d60d1aa2229fbb17c18ee97e5071fa
alexle-developer <alex.le.developer@gmail.com>
2024-03-24 11:20:35 -0500

    register the store (state) and effects inthe  app.config

--- apps/todo/todo-ngrx/src/app/app.config.ts
+++ apps/todo/todo-ngrx/src/app/app.config.ts
@@ -3,7 +3,12 @@ import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
+import { appEffects, appStore } from './+state/todo.state';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideEffects(), provideStore(), provideRouter(appRoutes)],
+  providers: [
+    provideEffects(appEffects), // register the effects with the NgRx store
+    provideStore(appStore), // register the reducers with the NgRx store
+    provideRouter(appRoutes),
+  ],
 };
